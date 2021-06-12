298dd2f0ae7d9233f7f80ac2cb823a9d049c5125
magnetotail <magnetotail@posteo.net>
2021-06-12 18:22:58 +0200

    Guests now spawn up to a fixed count and space between them

--- World.gd
+++ World.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 onready var streets = $Streets
+onready var used_cells = streets.get_used_cells()
 onready var guests = $Guests
 
 var Guest = load("res://Guest.tscn")
 
 var rng = RandomNumberGenerator.new()
+var spawn_tries = 0
 
 func _on_Guest_picked_up(destinationColor,name):
 	print('Picked Up %s with name %s' % [destinationColor,name])
@@ -14,14 +16,30 @@ func _ready():
 	rng.randomize()
 	pass 
 
-
+func _physics_process(delta):
+	pass
 
 func _on_GuestTimer_timeout():
-	var used_cells = streets.get_used_cells()
-	var new_guest_cell = used_cells[rng.randi_range(0, used_cells.size())]
-	var guest_position = streets.map_to_world(new_guest_cell)
-	print('new guest at: %s' % [guest_position])
-	var guest = Guest.instance();
-	guest.position = guest_position;
-	guests.add_child(guest)
-	print('Guest count: %s' % [guests.get_child_count()])
+	create_new_guest()
+	
+
+func create_new_guest():
+	if guests.get_children().size() >= 7:
+		return
+		
+	var position_found = false;
+	var new_guest_position
+	
+	while !position_found:
+		position_found = true;
+		var new_guest_cell = used_cells[rng.randi_range(0, used_cells.size()) -1]
+		new_guest_position = streets.map_to_world(new_guest_cell)
+		
+		for guest in guests.get_children():
+			if (new_guest_position - guest.position).length() < 300:
+				position_found = false
+				break
+			
+	var new_guest = Guest.instance()
+	new_guest.position = new_guest_position;
+	guests.add_child(new_guest)
