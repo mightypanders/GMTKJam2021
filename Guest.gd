84ee33474c9827c4c462db1c21d01f8449a75edf
magnetotail <magnetotail@posteo.net>
2021-06-12 21:54:29 +0200

    Guests now have a pickup time

--- Guest.gd
+++ Guest.gd
@@ -4,10 +4,13 @@ onready var pickUpArea = $PickUpArea
 export var guestName = "Dieter"
 export var  PICKUPTRESHOLD = 100
 export var destinationColor = Color.yellow
+var pickup_time
+
 signal picked_up(color,name)
 signal dropped_off_success 
 signal dropped_off_idle
 signal dropped_off_fail
+
 var rng = RandomNumberGenerator.new()
 onready var sprite = $Sprite
 onready var exclusionZoneShape = $ExclusionZone/CollisionShape2D
@@ -32,10 +35,7 @@ func _ready():
 	destinationColor = colorList[n]
 	#print(destinationColor)
 	sprite.modulate = destinationColor
-
-
-
-	pass # Replace with function body.
+	
 
 func _on_PickUpArea_body_entered(body):
 	print(body.name)
@@ -43,6 +43,7 @@ func _on_PickUpArea_body_entered(body):
 		print(body.velocity.length())
 		if body.velocity.length() <= PICKUPTRESHOLD:
 			emit_signal("picked_up",destinationColor,guestName)
+			pickup_time = OS.get_system_time_msecs()
 			# start pickup process
 
 		# we are being picked up by the player
